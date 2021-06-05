68251461286e08287dd1b621d4e95b617ec69b25
Soph <Sophistic4ted@gmail.com>
2021-06-05 16:19:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function() {
         var time = new Date();
         var hours = time.getHours();
         var minutes = time.getMinutes();
-        console.log(hours);
+        console.log(typeof(hours));
         console.log(minutes);
         $(".main").load("inject.html");
 }
