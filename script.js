46b91f5247dc8e053f71cc63562b64b15462d4aa
Soph <Sophistic4ted@gmail.com>
2021-06-05 02:36:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 let now = new Date();
 console.log(now);
+window.onload = function() {
+    if (window.jQuery) {  
+        // jQuery is loaded  
+        alert("Yeah!");
+    } else {
+        // jQuery is not loaded
+        alert("Doesn't Work");
+    }
+}
 $(".main").html("inject.html");
